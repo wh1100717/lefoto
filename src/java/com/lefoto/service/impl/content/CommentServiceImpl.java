45a2f1c8ba24aa94c3588148e36d5f535db7ac8e
wh1100717 <eric.3126@hotmail.com>
2012-12-16 01:45:16 +0800

    更新评论代码，更改为@方式的评论

--- src/java/com/lefoto/service/impl/content/CommentServiceImpl.java
+++ src/java/com/lefoto/service/impl/content/CommentServiceImpl.java
@@ -4,10 +4,15 @@
  */
 package com.lefoto.service.impl.content;
 
+import com.lefoto.common.cache.UserCache;
 import com.lefoto.dao.iface.content.CommentDao;
 import com.lefoto.model.content.LeComment;
 import com.lefoto.service.iface.content.CommentService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +29,10 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public void addComment(LeComment comment) {
+        Map<String, String> result = parseCommentContent(comment);
+        comment.setContent(result.get("content"));
+        //TODO 需要去做对被At的用户的处理
+
         commentDao.addComment(comment);
     }
 
@@ -45,5 +54,38 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<LeComment> getCommentsAjax(int objectType, int objectId, int lastCommentId, int size) {
         return commentDao.getCommentsAjax(objectType, objectId, lastCommentId, size);
+    }
+
+    private Map<String, String> parseCommentContent(LeComment comment) {
+        Map userMap = new HashMap();
+        Map<String, String> result = new HashMap<String, String>();
+        String content = comment.getContent();
+        String newContent = content;
+        String atUserNames = "";
+        int objectType = comment.getObjectType();
+        int objectId = comment.getObjectId();
+        List<LeComment> comments = commentDao.getComments(objectType, objectId);
+        for (LeComment leComment : comments) {
+            userMap.put(leComment.getUserName(), 1);
+        }
+        //TODO 需要把该用户的好友添加到userMap中
+        String regex = "(@[^ ]*)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(content);
+        while (match.find()) {
+            String userName = match.group();
+            System.out.println(userName);
+            if (userMap.get(userName) != null) {
+                newContent = newContent.replace("@" + userName, "[at]@" + userName + "[/at]");
+                if (atUserNames.equals("")) {
+                    atUserNames = userName;
+                } else {
+                    atUserNames = atUserNames + "," + userName;
+                }
+            }
+        }
+        result.put("content", newContent);
+        result.put("atUserNames", atUserNames);
+        return result;
     }
 }
